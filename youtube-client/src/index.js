0d8a0115dd7e53b57a8b9a081124c2d51b55a543
saniok017 <saniok017@gmail.com>
2018-11-16 16:20:33 +0300

    youtube-client/feat: - lodash experiment

--- youtube-client/src/index.js
+++ youtube-client/src/index.js
@@ -1,1 +1,3 @@
-const babel = 'hi';
+let searchForm = '<form class="form-wrapper"><input type="text" id="search" placeholder="Search for..." required><input type="submit" value="go" id="submit"></form>';
+
+document.getElementsByTagName('body').innerHTML += _.template(searchForm);
