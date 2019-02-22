2621c3d79b5c97ba2000e39e4140fab3d68b412b
saniok017 <saniok017@gmail.com>
2019-02-22 17:35:38 +0300

    mentor-dashboard/feat: - add test

--- mentor-dashboard/__tests__/render.test.js
+++ mentor-dashboard/__tests__/render.test.js
@@ -2,6 +2,7 @@ import { mount } from 'enzyme';
 import Select from 'react-select';
 import Table from '../components/Table/Table';
 import About from '../pages/about';
+import Index from '../pages/index';
 
 const object = {
   mentorGitHub: '',
@@ -21,5 +22,9 @@ describe('Components', () => {
   it('should render without throwing an error', () => {
     const wrapper = mount(<About mentors={ [object] } />);
     expect(wrapper.find('h1').text()).toBe(' Rolling scopes school 2018Q3 Mentors-list ');
+  });
+  it('should render without throwing an error', async () => {
+    const wrapper = await mount(<Index options={ { options: [], tasks: [] } } />);
+    expect(wrapper.find('Select').text()).toBe('Select...');
   });
 });
