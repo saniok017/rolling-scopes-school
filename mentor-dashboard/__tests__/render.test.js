d2d61f291a368c3318a65ba16dd503f77d374531
saniok017 <saniok017@gmail.com>
2019-02-22 14:01:18 +0300

    mentor-dashboard/feat: - add tests

--- mentor-dashboard/__tests__/render.test.js
+++ mentor-dashboard/__tests__/render.test.js
@@ -1,9 +1,25 @@
 import { mount } from 'enzyme';
+import Select from 'react-select';
 import Table from '../components/Table/Table';
+import About from '../pages/about';
 
-describe('Pages', () => {
+const object = {
+  mentorGitHub: '',
+  mentorFullName: '',
+};
+
+
+describe('Components', () => {
   it('should render without throwing an error', () => {
     const wrapper = mount(<Table mentorName={ 'name' } data={{ studentNickName: [] }} tasks={[]} />);
     expect(wrapper.find('Table').text()).toBe('namestudentNickName');
+  });
+  it('should render without throwing an error', () => {
+    const wrapper = mount(<Select options={[]} />);
+    expect(wrapper.find('Select').text()).toBe('Select...');
+  });
+  it('should render without throwing an error', () => {
+    const wrapper = mount(<About mentors={ [object] } />);
+    expect(wrapper.find('h1').text()).toBe(' Rolling scopes school 2018Q3 Mentors-list ');
   });
 });
--- mentor-dashboard/babel.config.js
+++ mentor-dashboard/babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    'syntax-dynamic-import',
+    'transform-async-to-generator',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-transform-runtime',
+  ],
+  env: {
+    production: {
+      presets: ['react-optimize'],
+    },
+  },
 };
